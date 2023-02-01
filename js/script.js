8d7d6d09c66c43d4a397f294c46928afbd836877
wojnoski <karol.wojnarowski@gmail.com>
2023-02-01 12:53:48 +0100

    Move scripts again

--- js/script.js
+++ js/script.js
@@ -9,7 +9,23 @@ if(randomNumber == 1){
 } else if (randomNumber == 2){
     computerMove = 'papier';
 } else if (randomNumber == 3){
-    computerMove = 'nozyce';
+    computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if (playerInput == '2'){
+    playerMove = 'papier';
+} else if (playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
